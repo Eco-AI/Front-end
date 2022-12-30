bdba4f9a66d69f8eb9fdf2f187c750394ed7fb92
CloudRider <smlxmpr01@gmail.com>
2022-12-30 01:11:19 +0100

    modified:   src/router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -120,7 +120,7 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/TrashInfoView.vue')
-    }*/
+    }
   ]
 })
 
