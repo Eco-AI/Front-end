9e3a8fc05ebc2e6311d47935e38d40a67a28f7c9
Christian <chriven321@gmail.com>
2023-01-01 11:54:38 +0100

    modifica package.json

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
+  /*resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
@@ -14,5 +14,5 @@ export default defineConfig({
   base: process.env.BASE_PATH || '/'
   // base: process.env.NODE_ENV === 'production'
   //         ? '/EasyLibApp/' // prod
-  //         : '/', // dev
+  //         : '/', // dev*/
 })
